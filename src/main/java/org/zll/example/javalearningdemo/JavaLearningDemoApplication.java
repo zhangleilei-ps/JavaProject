4d8041b300363a10cea515adaccb22584a10a688
zll <18638656101@163.com>
2026-02-15 23:06:09 +0800

    add new project

---
+++ src/main/java/org/zll/example/javalearningdemo/JavaLearningDemoApplication.java
@@ -0,0 +1,13 @@
+package org.zll.example.javalearningdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLearningDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaLearningDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/zll/example/javalearningdemo/JavaLearningDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zll.example.javalearningdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLearningDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
