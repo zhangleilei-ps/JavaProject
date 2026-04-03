4645000bdf8d48b0ae90188b2595731f01cb28d5
zll <18638656101@163.com>
2026-04-03 16:44:47 +0800

    add project run content

--- src/main/java/org/zll/example/javalearningdemo/JavaLearningDemoApplication.java
+++ src/main/java/org/zll/example/javalearningdemo/JavaLearningDemoApplication.java
@@ -1,17 +1,23 @@
 package org.zll.example.javalearningdemo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-
+@Slf4j
 @SpringBootApplication
 @MapperScan("org.zll.example.javalearningdemo.mapper")
-public class JavaLearningDemoApplication {
+public class JavaLearningDemoApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(JavaLearningDemoApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("=================启动成功================");
+    }
 }
